6e5906661d7feaacc7f0d621d1b0705b08df6004
Junxi Li <jimkeecn@gmail.com>
2019-06-18 16:07:34 +1000

    initial the app

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+  import firebase from 'firebase';
+  // Your web app's Firebase configuration
+
+  const config = {
+    apiKey: "AIzaSyBRa8oFa-T8lyz8aYomlNJeQEIwh292IvA",
+    authDomain: "react-firebase-authentic-86280.firebaseapp.com",
+    databaseURL: "https://react-firebase-authentic-86280.firebaseio.com",
+    projectId: "react-firebase-authentic-86280",
+    storageBucket: "",
+    messagingSenderId: "780158411654",
+    appId: "1:780158411654:web:4ef50e2325c30c41"
+  }
+  // Initialize Firebase
+  const fire = firebase.initializeApp(config);
+
+  export default fire;
+
